649328071b949af7bcda25bc952a5ff821d860e6
vandana gowda <vandanaumeshpatel@gmail.com>
2025-12-11 11:03:29 +0530

    second commit

--- src/collection1.java
+++ src/collection1.java
@@ -5,9 +5,9 @@ public class collection1 {
     public static void main(String[] args){
         Collection<String> names=new ArrayList<>();
         Collection<Integer> age=new ArrayList<>();
-        names.add("vandu");
-        names.add("vanduu");
-        names.add("vanduuu");
+        names.add("vandu   2");
+        names.add("vanduu   3");
+        names.add("vanduuu  4");
         age.add(5);
         System.out.println(names);
         System.out.println(age);
